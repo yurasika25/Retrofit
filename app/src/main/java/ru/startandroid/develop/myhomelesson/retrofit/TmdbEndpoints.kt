7e1916241cd8b1d4c90afc56e7bb6bbfbc1841e3
yurasika25 <yrasika80@gmail.com>
2020-10-01 19:27:25 +0300

    Спроба зробити запити через ретрофіт

---
+++ app/src/main/java/ru/startandroid/develop/myhomelesson/retrofit/TmdbEndpoints.kt
@@ -0,0 +1,12 @@
+package ru.startandroid.develop.myhomelesson.retrofit
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface TmdbEndpoints {
+
+    @GET("/3/movie/popular")
+    fun getMovies(@Query("api_key") key: String): Call<PopularMovies>
+
+}
